309a329723f9a870dbd7a56920a1c4e1b20e761c
Ryan Magoon <mr.magoon5@gmail.com>
2019-04-27 16:31:15 -0400

    setup initial login screen tests

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,5 +1,25 @@
-// Basic unit test
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '../login'
+
 test('calls onSubmit with the username and password when submitted', () => {
+  const handleSubmit = jest.fn()
+  const container = document.createElement('div')
+  ReactDOM.render(<Login onSubmit={handleSubmit} />, container)
+
+  const form = container.querySelector('form')
+  const {username, password} = form.elements
+  username.value = 'chucknorris'
+  password.value = 'I do not need a password'
+
+  form.dispatchEvent(new window.Event('submit'))
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username.value,
+    password: password.value,
+  })
+
   // Arrange
   // 🐨 create a fake object to hold the form field values (username and password)
   // 🐨 create a jest.fn() for your submit handler
