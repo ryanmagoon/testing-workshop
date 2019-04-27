83ed7208a687adf0a21725418a6e96589b2f303a
Ryan Magoon <mr.magoon5@gmail.com>
2019-04-27 14:00:38 -0400

    mock stuff

--- client/src/screens/__tests__/editor.todo.js
+++ client/src/screens/__tests__/editor.todo.js
@@ -1,4 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Editor from '../editor.todo'
+
+jest.mock('../../utils/api', () => ({
+  posts: {
+    create: jest.fn(() => Promise.resolve())
+  }
+}))
+
 test('calls onSubmit with the username and password when submitted', () => {
+  const container = document.createElement('div')
+  const fakeUser = { id: 'foobar' }
+  const fakeHistory = {
+    push: jest.fn(),
+  }
+  ReactDOM.render(<Editor user={fakeUser} history={fakeHistory} />, container)
+  const form = container.querySelector('form')
+  const { title, content, tags } = form.elements
+  title.value = 'I like twix'
+  content.value = 'Like a lot.. Sorta'
+  tags.value = 'twix,     my  ,likes'
+
+  const submit = new window.Event('submit')
+  form.dispatchEvent(submit)
+
+
+  // const title = container.
   // Arrange
   // create a fake user, post, history, and api
   //
